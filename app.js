a3f4fdf121f083fc61707c54cf58a295078c68c4
scorpiraq <96728173+scorpiraq@users.noreply.github.com>
2023-10-27 23:07:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use("/api", (req, res, next) =>{
+    res.send("Hello, World")
+})
+
+mongoose.
+connect(
+    "mongodb+srv://omermohammedabbas99:5UZmKnYEWEN8YFFI@cluster0.aho3kn6.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(()=>app.listen(5000))
+    .then(()=>
+    console.log("Connected to DB & listeing to Localhost")
+    )
+    .catch((err)=>console.log(err));
+//P43NNYTKcB57US4G
